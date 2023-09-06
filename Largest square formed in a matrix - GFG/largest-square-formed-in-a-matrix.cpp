4cf5e964d9ca4da18645231e8211592d3cab0c9c
Angad Singh Brar <97975920+angadbrarx@users.noreply.github.com>
2023-09-06 13:40:12 +0530

    Added solution - LeetHub

--- Largest square formed in a matrix - GFG/largest-square-formed-in-a-matrix.cpp
+++ Largest square formed in a matrix - GFG/largest-square-formed-in-a-matrix.cpp
@@ -51,39 +51,46 @@ public:
             return dp[i][j] = 0;
     }
     
-    // int solveTab(vector<vector<int>> &mat, int &maximum){
-    //     vector< vector<int> >dp(row + 1, vector<int>(col + 1, 0));
+    int solveTab(vector<vector<int>> &mat, int &maximum){
         
-    //     for(int i = row-1; i>=0; i--){
-    //         for(int j = col-1; j>=0; j--){
-    //             int right = dp[i][j+1];
-    //             int diagonal = dp[i+1][j+1];
-    //             int down = dp[i+1][j];
+        int row = mat.size();
+        int col = mat[0].size();
+        
+        vector< vector<int> >dp(row + 1, vector<int>(col + 1, 0));
+        
+        for(int i = row-1; i>=0; i--){
+            for(int j = col-1; j>=0; j--){
+                int right = dp[i][j+1];
+                int diagonal = dp[i+1][j+1];
+                int down = dp[i+1][j];
                 
                  
-    //             if(mat[i][j] == 1){
-    //                 dp[i][j] = 1 + min(right, min(diagonal, down));
-    //                 maximum = max(maximum, dp[i][j]);
+                if(mat[i][j] == 1){
+                    dp[i][j] = 1 + min(right, min(diagonal, down));
+                    maximum = max(maximum, dp[i][j]);
                   
-    //             }
-    //             else
-    //               dp[i][j] = 0;
-    //         }
-    //     }
-    //     return dp[0][0];
+                }
+                else
+                  dp[i][j] = 0;
+            }
+        }
+        return dp[0][0];
         
-    // }
+    }
     
     int maxSquare(int n, int m, vector<vector<int>> mat){
         // int maxi = 0;
         // solveRec(mat, 0, 0, maxi);
         // return maxi;
         
-        vector<vector<int>>dp(n+1, vector<int>(m+1, -1));
-        int maximum = 0;
-        solveMem(mat, 0, 0, maximum, dp);
-        return maximum;
+        // vector<vector<int>>dp(n+1, vector<int>(m+1, -1));
+        // int maximum = 0;
+        // solveMem(mat, 0, 0, maximum, dp);
+        // return maximum;
         
+        int maximum = 0;
+        solveTab(mat, maximum);
+        return maximum;
     }
 };
 
